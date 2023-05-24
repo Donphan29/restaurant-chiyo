5aa7b20b611228ca471c0ff1bc380f644ec7f2cc
Don Phan <huy-don.phan@polymtl.ca>
2023-05-24 12:37:52 -0400

    Nav Title and Menu Button

---
+++ restaurant-chiyo/components/Button.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { Button } from "@mui/material";
+
+export default function MenuButton() {
+    return (
+        <>
+            <Button variant="contained">Menu</Button>
+        </>
+    )
+}
--- restaurant-chiyo/components/Nav.tsx
+++ restaurant-chiyo/components/Nav.tsx
@@ -1,19 +1,24 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import MenuButton from './Button';
 
 export default function Nav() {
     return (
-        <nav className='flex-between w-full mb-16 pt-3 pl-3'>
-            <Link href='/' className='flex gap-2 flex-center'>
+        <nav className='w-full pt-3 pl-3 pb-3 pr-3 flex flex-row justify-between'>
+            <Link href='/' className='flex gap-2'>
                 <Image
                     src='/assets/icons/LEGO_logo.svg.png'
                     alt='chiyo logo'
-                    width={30}
-                    height={30}
+                    width={35}
+                    height={35}
                     className='object-contain'
                 />
-                <p>Restaurant Chiyo</p>
+                <div className='flex flex-col justify-center md:flex-row md:gap-x-2 md:items-center'>
+                    <h1>Restaurant</h1>
+                    <h2>Chiyo</h2>
+                </div>
             </Link>
+            <MenuButton></MenuButton>
         </nav>
     )
 }
