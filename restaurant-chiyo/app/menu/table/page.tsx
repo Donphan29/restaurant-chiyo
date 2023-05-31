fbaf6ecd45429ea3af3a26bbdb5da750603c4185
Don Phan <huy-don.phan@polymtl.ca>
2023-05-31 18:34:20 -0400

    Implemented get menu items from mongodb

--- restaurant-chiyo/app/menu/table/page.tsx
+++ restaurant-chiyo/app/menu/table/page.tsx
@@ -1,6 +1,16 @@
-import MenuItem from "@components/general/MenuItem"
+import getTableDhote from '@lib/table';
+import MenuItem from '@components/general/MenuItem';
 
-export default function Table() {
+async function getMenuItems() {
+    const items = await getTableDhote();
+    if (!items) throw new Error('failed to fetch table dhote items');
+
+    return items;
+}
+
+export default async function Table() {
+    const items = await getMenuItems();
+
     return (
         <section className='w-full h-screen'>
             <div
@@ -19,25 +29,10 @@ export default function Table() {
                     <p className='text-sm'><i>Imperialroll (hot) or chicken springroll (cold)</i></p>
                 </section>
 
-                <section className='w-5/6 flex flex-row justify-around pt-4'>
-                    <section className='w-1/3 flex flex-col justify-stretch gap-5'>
-                        <MenuItem name='Poulet, crevettes et légumes sautés servi sur nouilles croustillantes' english='Chicken, shrimps, vegetables stir fry served on crispy noodles' price={28}></MenuItem>
-                        <MenuItem name='Poulet à la sauce aux arachides et épinards croustillants servi avec choix de riz ou de vermicelles' english='Peanut butter chicken served with spinach crisps and choice of rice or vermicelli' price={28}></MenuItem>
-                        <MenuItem name='Combinaison de fruits de mer à la sauce au cari rouge servis avec choix de riz ou de vermicelles' english='Red curry seafood combination served with choice of rice or vermicelli' price={35}></MenuItem>
-                        <MenuItem name="Grillades de poulet et crevettes à l'asiatique servi avec salade et choix de riz ou de vermicelles" english='Asian style grilled chicken and shrimps served with salad and choice of rice or vermicelli' price={28}></MenuItem>
-                        <MenuItem name="Trio de grillades a l'asiatique (poulet, boeuf, crevette) servi avec salade et choix de riz ou de vermicelles" english='Asian style grilled chicken, beef and shrimps served with salad and choice of rice or vermicelli' price={32}></MenuItem>
-                        <MenuItem name='Sizzling de fruit de mer et légumes avec choix de riz ou de vermicelles' english='Sea food sizzling served with choice of rice or vermicelli' price={36}></MenuItem>
-                    </section>
-
-                    <section className='w-1/3 flex flex-col justify-stretch gap-5'>
-                        <MenuItem name='Boeuf et légumes sautés servi sur nouilles croustillantes' english='Beef and vegetables stir fry served on crispy noodles' price={28}></MenuItem>
-                        <MenuItem name='Crevettes sautées aux légumes à la sauce au aigre douce servi avec choix de riz ou de vermicelles' english='Sweet and sour shrimps and vegetables served with choice of rice or vermicelli' price={30}></MenuItem>
-                        <MenuItem name="Grillades de poulet et de boeuf à l'asiatique servi avec salade et choix de riz ou de vermicelles" english='Asian style grilled chicken and beef served with salad and choice of rice or vermicelli' price={28}></MenuItem>
-                        <MenuItem name="Grillades de boeuf et crevettes à l'asiatique servi avec salade et choix de riz ou de vermicelles" english='Asian style grilled beef and shrimps served with salad and choice of rice or vermicelli' price={29}></MenuItem>
-                        <MenuItem name='Teriyaki sur un lit de légumes sautés avec choix de riz ou de vermicelles' english='Teriyaki on a sautéed vegetables bed served with choice of rice or vermicelli' price={34}></MenuItem>
-                        <MenuItem name='Général Tao avec choix de riz ou de vermicelles' english='General Tao with choice of rice or vermicelli' price={28}></MenuItem>
-                    </section>
-
+                <section className='w-5/6 grid grid-cols-2 grid-rows-2 justify-around gap-5 pt-4'>
+                    {items.table_dhote?.map((item) => {
+                        return (<MenuItem name={item.name} english={item.english} price={item.price} ></MenuItem>)
+                    })}
                 </section>
             </section>
         </section>
