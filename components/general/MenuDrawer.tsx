e840031601fdf93af16ab014764110556ab1b280
Don Phan <huy-don.phan@polymtl.ca>
2023-06-15 11:35:25 -0400

    Added minimize icon and larger text

--- components/general/MenuDrawer.tsx
+++ components/general/MenuDrawer.tsx
@@ -2,13 +2,13 @@
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
+import Drawer from '@mui/material/Drawer';
 import Link from '@mui/material/Link';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
+import MinimizeIcon from '@mui/icons-material/Minimize';
 
 import { MENUS } from '@constants/Constants';
 
@@ -38,18 +38,19 @@ export default function MenuDrawer() {
             onClick={toggleDrawer(anchor, false)}
             onKeyDown={toggleDrawer(anchor, false)}
         >
-            <List sx={{ my: '15%' }}>
+            <div className='flex justify-end mt-4 mr-8'><MinimizeIcon fontSize='large' color='error'></MinimizeIcon></div>
+            <List sx={{ mb: '15%' }}>
                 <ListItem key='home' disablePadding>
                     <ListItemButton className='justify-center'>
-                        <Link href='/' underline='none' color='#ffff' className='font-open'>
+                        <Link href='/' underline='none' color='#ffff' className='font-open text-xl'>
                             Accueil
                         </Link>
                     </ListItemButton>
                 </ListItem>
                 {MENUS.map((menu) => (
-                    <ListItem key={menu.menu} disablePadding sx={{ mt: '3%' }}>
+                    <ListItem key={menu.menu} disablePadding sx={{ mt: '8%' }}>
                         <ListItemButton className='justify-center'>
-                            <Link href={'/menu/' + menu.menu} underline='none' color='#ffff' className='font-open text-center'>
+                            <Link href={'/menu/' + menu.menu} underline='none' color='#ffff' className='font-open text-center text-xl'>
                                 {menu.title}
                             </Link>
                         </ListItemButton>
