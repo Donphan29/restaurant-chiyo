4ef108c314ea261911b7e26c4a4b467b6364a6a8
Don Phan <huy-don.phan@polymtl.ca>
2023-07-14 14:47:52 -0400

    Adjusted padding for footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -4,7 +4,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 
 export default function Footer() {
     return (
-        <section className='w-full h-1/6 p-7 pt-14 flex flex-wrap justify-between content-center text-slate-200 text-xs'>
+        <section className='w-full h-1/6 pl-7 pr-7 pt-14 pb-3 flex flex-wrap justify-between content-center text-slate-200 text-xs'>
             <p className='font-source'><i>@ 2023 Restaurant Chiyo</i></p>
             <a href='https://www.facebook.com/profile.php?id=100057120744279'><FacebookIcon></FacebookIcon></a>
         </section>
